ac697d10d55b6bead8832ae3f80ffdabc787124a
Stavros Skamagkis <stavrossk@gmail.com>
2013-06-06 06:36:19 +0300

    Create Form events: blur.js
    The onBlur event fires whenever a form field lose focus.

---
+++ Form events: blur.js
@@ -0,0 +1,20 @@
+
+
+addEvent
+       (
+       
+       
+          document.getElementById('myinput'),
+          
+          'blur',
+          
+          function(event) 
+          {
+
+            alert('The element lost focus (blurred)');
+
+
+          }
+        
+        
+        );
