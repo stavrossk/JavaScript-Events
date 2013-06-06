8400fa18adfe37dbe7b982fc4a329b77c02023e1
Stavros Skamagkis <stavrossk@gmail.com>
2013-06-06 06:15:05 +0300

    Update Page events.js

--- Page events.js
+++ Page events.js
@@ -1,3 +1,4 @@
+
 addEvent(window, 'load',
           function(event) 
           {
@@ -6,4 +7,48 @@ addEvent(window, 'load',
 
           }
 
+        );
+
+
+
+
+
+addEvent(window, 'resize',
+           function(event) 
+           {
+
+              alert('The page has been resized');
+
+           }
+
+         );
+
+
+
+
+1
+addEvent(window, 'scroll', 
+           function(event) 
+           {
+ 
+              alert('The page is being scrolled');
+ 
+           }
+          
+         );
+
+
+
+
+
+
+addEvent(window, 'unload',
+           function(event) 
+           {
+                    
+              alert('The user left the page');
+    
+
+           }
+      
         );
